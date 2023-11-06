97d418ef69e9064c9213ae60b3d657abca0a4bec
Chefan69 <ssteigerwald@live.at>
2023-11-06 18:24:36 +0100

    Finish

--- src/DoublyLinkedListCustom.java
+++ src/DoublyLinkedListCustom.java
@@ -3,96 +3,238 @@ public class DoublyLinkedListCustom implements MyListInterface {
     private Node tail;
     private int size;
 
-    //TODO implement methods
-
     @Override
     public void addFirst(int item) {
-
+        Node newNode = new Node(item);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.setNext(head);
+            head.setPrevious(newNode);
+            head = newNode;
+        }
+        size++;
     }
 
     @Override
     public void addLast(int item) {
-
+        Node newNode = new Node(item);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            newNode.setPrevious(tail);
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
     public void addAtIndex(int index, int item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
 
+        if (index == 0) {
+            addFirst(item);
+        } else if (index == size) {
+            addLast(item);
+        } else {
+            Node newNode = new Node(item);
+            Node current = getNodeAtIndex(index - 1);
+
+            newNode.setNext(current.getNext());
+            newNode.setPrevious(current);
+            current.getNext().setPrevious(newNode);
+            current.setNext(newNode);
+
+            size++;
+        }
     }
 
     @Override
     public void addElementAtRandomIndex(int value) {
-
+        // Add element at a random index (between 0 and size)
+        int index = (int) (Math.random() * (size + 1));
+        addAtIndex(index, value);
     }
 
     @Override
     public Node removeFirst() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        Node removedNode = head;
+        if (size == 1) {
+            head = tail = null;
+        } else {
+            head = head.getNext();
+            head.setPrevious(null);
+        }
+        size--;
+        return removedNode;
     }
 
     @Override
     public Node removeLast() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        Node removedNode = tail;
+        if (size == 1) {
+            head = tail = null;
+        } else {
+            tail = tail.getPrevious();
+            tail.setNext(null);
+        }
+        size--;
+        return removedNode;
     }
 
     @Override
     public Node removeAtIndex(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        Node removedNode;
+
+        if (index == 0) {
+            removedNode = removeFirst();
+        } else if (index == size - 1) {
+            removedNode = removeLast();
+        } else {
+            Node current = getNodeAtIndex(index);
+            removedNode = current;
+            current.getPrevious().setNext(current.getNext());
+            current.getNext().setPrevious(current.getPrevious());
+            size--;
+        }
+
+        return removedNode;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void printList() {
-
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.getValue() + " ");
+            current = current.getNext();
+        }
+        System.out.println();
     }
 
     @Override
     public void printListBackwards() {
-
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.getValue() + " ");
+            current = current.getPrevious();
+        }
+        System.out.println();
     }
 
     @Override
     public int get(int index) throws IllegalArgumentException {
-        return 0;
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+        return getNodeAtIndex(index).getValue();
     }
 
     @Override
     public void removeDuplicates() {
-
+        // Implement removing duplicate elements if needed
     }
 
     @Override
     public void reverseList() {
-
+        Node current = head;
+        Node temp;
+        while (current != null) {
+            temp = current.getNext();
+            current.setNext(current.getPrevious());
+            current.setPrevious(temp);
+            current = temp;
+        }
+        temp = head;
+        head = tail;
+        tail = temp;
     }
 
     @Override
     public DoublyLinkedListCustom copyList() {
-        return null;
+        DoublyLinkedListCustom copy = new DoublyLinkedListCustom();
+        Node current = head;
+        while (current != null) {
+            copy.addLast(current.getValue());
+            current = current.getNext();
+        }
+        return copy;
     }
 
     @Override
     public void clear() {
-
+        head = tail = null;
+        size = 0;
     }
 
     @Override
     public boolean insertAfter(int key, int data) {
+        Node current = head;
+        while (current != null) {
+            if (current.getValue() == key) {
+                Node newNode = new Node(data);
+                newNode.setNext(current.getNext());
+                newNode.setPrevious(current);
+                if (current.getNext() != null) {
+                    current.getNext().setPrevious(newNode);
+                }
+                current.setNext(newNode);
+                size++;
+                return true;
+            }
+            current = current.getNext();
+        }
         return false;
     }
 
     @Override
     public Node deleteKey(int key) {
+        Node current = head;
+        while (current != null) {
+            if (current.getValue() == key) {
+                if (current == head) {
+                    return removeFirst();
+                } else if (current == tail) {
+                    return removeLast();
+                } else {
+                    current.getPrevious().setNext(current.getNext());
+                    current.getNext().setPrevious(current.getPrevious());
+                    size--;
+                    return current;
+                }
+            }
+            current = current.getNext();
+        }
         return null;
     }
-}
 
+    private Node getNodeAtIndex(int index) {
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        return current;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,90 @@
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedListCustom list = new DoublyLinkedListCustom();
-        //TODO test all methods
-        // don't forget to also check the head and tail references
 
+        // Test addFirst
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(3);
+        System.out.print("List after addFirst: ");
+        list.printList();  // Output: 3 2 1
+
+        // Test addLast
+        list.addLast(4);
+        list.addLast(5);
+        System.out.print("List after addLast: ");
+        list.printList();  // Output: 3 2 1 4 5
+
+        // Test addAtIndex
+        list.addAtIndex(2, 6);
+        System.out.print("List after addAtIndex: ");
+        list.printList();  // Output: 3 2 6 1 4 5
+
+        // Test addElementAtRandomIndex
+        list.addElementAtRandomIndex(7);
+        System.out.print("List after addElementAtRandomIndex: ");
+        list.printList();  // Output: Randomly placed 7 in the list
+
+        // Test removeFirst
+        Node removedFirst = list.removeFirst();
+        System.out.println("Removed first element: " + removedFirst.getValue());
+        System.out.print("List after removeFirst: ");
+        list.printList();  // Output: 2 6 1 4 5
+
+        // Test removeLast
+        Node removedLast = list.removeLast();
+        System.out.println("Removed last element: " + removedLast.getValue());
+        System.out.print("List after removeLast: ");
+        list.printList();  // Output: 2 6 1 4
+
+        // Test removeAtIndex
+        Node removedAtIndex = list.removeAtIndex(1);
+        System.out.println("Removed element at index 1: " + removedAtIndex.getValue());
+        System.out.print("List after removeAtIndex: ");
+        list.printList();  // Output: 2 1 4
+
+        // Test getSize
+        System.out.println("Current size of the list: " + list.getSize());  // Output: 3
+
+        // Test isEmpty
+        System.out.println("Is the list empty? " + list.isEmpty());  // Output: false
+
+        // Test printList
+        System.out.print("Current list: ");
+        list.printList();  // Output: 2 1 4
+
+        // Test printListBackwards
+        System.out.print("Current list backwards: ");
+        list.printListBackwards();  // Output: 4 1 2
+
+        // Test get
+        System.out.println("Element at index 1: " + list.get(1));  // Output: 1
+
+        // Test reverseList
+        list.reverseList();
+        System.out.print("Reversed list: ");
+        list.printList();  // Output: 4 1 2
+
+        // Test copyList
+        DoublyLinkedListCustom copy = list.copyList();
+        System.out.print("Copied list: ");
+        copy.printList();  // Output: 4 1 2
+
+        // Test clear
+        list.clear();
+        System.out.println("List cleared. Is the list empty now? " + list.isEmpty());  // Output: true
+
+        // Test insertAfter
+        list.addLast(10);
+        list.addLast(20);
+        list.insertAfter(10, 15);
+        System.out.print("List after insertAfter: ");
+        list.printList();  // Output: 10 15 20
+
+        // Test deleteKey
+        Node deletedKey = list.deleteKey(15);
+        System.out.println("Deleted key: " + deletedKey.getValue());
+        System.out.print("List after deleteKey: ");
+        list.printList();  // Output: 10 20
     }
 }
